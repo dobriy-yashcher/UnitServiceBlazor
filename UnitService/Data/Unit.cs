8dab333e16dc66dc1f3cbf022459e0d0cf51499b
dobriy-yashcher <golovinsergey17@gmail.com>
2022-10-19 20:32:21 +0300

    Add Unit, UnitService.

---
+++ UnitService/Data/Unit.cs
@@ -0,0 +1,11 @@
+﻿namespace UnitService.Data
+{
+    public class Unit
+    {
+        public string? Name { get; set; }
+
+        public DateTime DateBirthDay { get; set; }
+
+        public int Age => DateTime.Now.Year - DateBirthDay.Year;
+    }
+}
---
+++ UnitService/Data/UnitService.cs
@@ -0,0 +1,36 @@
+﻿namespace UnitService.Data
+{
+    public class UnitService
+    {
+        private static readonly string[] Names = new[]
+        {
+            "Shamil",
+            "Kamil",
+            "Roman",
+            "Bulat",
+            "Riaz",
+            "Rinat",
+            "Radmir",
+            "Nikita",
+            "Danil",
+            "Ilgam"
+        };
+
+        public Task<Unit[]> GetForecastAsync()
+        {
+            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new Unit
+            {
+                Name = Names[Random.Shared.Next(Names.Length)],
+                DateBirthDay = RandomDay()
+            }).ToArray());
+        }
+
+        private static DateTime start = new DateTime(1995, 1, 1);
+        private static int range = (DateTime.Now - new DateTime(1995, 1, 1)).Days;
+
+        private static DateTime RandomDay()
+        {
+            return start.AddDays(Random.Shared.Next(range));
+        }
+    }
+}
